96ea8c834277b7953ed079d7aa62b4f0f8fc2225
qkrrkdtj <149243446+qkrrkdtj@users.noreply.github.com>
2026-02-16 14:26:50 +0900

    [Bronze I] Title: 소인수분해, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.c
+++ 백준/Bronze/11653. 소인수분해/소인수분해.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 
-
 int main(void) {
-	int n;
+    int n;
+    scanf("%d", &n);
 
-	scanf("%d", &n);
+    for (int i = 2; i * i <= n; i++) {
+        while (n % i == 0) {
+            printf("%d\n", i);
+            n /= i;
+        }
+    }
 
-	while (n > 1) {
-		for (int i = 2; i <= n; i++) {
-			if (n % i == 0) {
-				n /= i;
-				printf("%d\n", i);
-				break;
-			}
-		}
-	}
+    if (n > 1) {
+        printf("%d\n", n);
+    }
 
-	
-	return 0;
+    return 0;
 }
